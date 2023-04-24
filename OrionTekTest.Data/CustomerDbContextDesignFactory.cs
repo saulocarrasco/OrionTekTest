26d83489438a447c655dbd7588c9d999b09ed547
saulocarrasco <sjesuscarrasco@gmail.com>
2023-04-24 14:30:34 -0400

    include configuration to run migrations

---
+++ OrionTekTest.Data/CustomerDbContextDesignFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace OrionTekTest.Data
+{
+    public class CustomerDbContextDesignFactory : IDesignTimeDbContextFactory<CustomersDbContext>
+    {
+        public CustomersDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            var builder = new DbContextOptionsBuilder<CustomersDbContext>();
+
+
+            var connectionString = configuration.GetConnectionString("SqlDataConnectionString");
+            builder.UseSqlServer(connectionString);
+
+            return new CustomersDbContext(builder.Options);
+        }
+    }
+}
